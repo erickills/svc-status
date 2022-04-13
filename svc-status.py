973e9fe5949dce07b07552dba314d053e654e08a
erickills <38625756+erickills@users.noreply.github.com>
2022-04-13 13:30:22 +0800

    Initial commit

---
+++ svc-status.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+from subprocess import check_output
+from re import search
+import csv
+
+cmd = 'systemctl --type=service'
+services = check_output(cmd.split()).decode().splitlines()[1:]
+status = []
+
+for i in services:
+    match = search(r'(^.*)loaded.*(running|exited|failed)',i)
+    if match:
+        status.append(list(match.groups()))
+
+status = sorted(status, key = lambda x: x[1], reverse = True)
+
+with open('/home/eric/projects/svc-status/status-results.csv','w') as file:
+    writer = csv.writer(file)
+    writer.writerows(status)
