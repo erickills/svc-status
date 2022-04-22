941a5c4515598c9cba21584015a1f9d010e8b6ba
erickills <38625756+erickills@users.noreply.github.com>
2022-04-22 13:19:31 +0800

    added comment

--- svc-status.py
+++ svc-status.py
@@ -9,12 +9,12 @@ services = check_output(cmd.split()).decode().splitlines()[1:]
 status = []
 
 for i in services:
-    match = search(r'(^.*)loaded.*(running|exited|failed)',i)
+    match = search(r'(^.*)loaded.*(running|exited|failed)',i) # status
     if match:
         status.append(list(match.groups()))
 
 status = sorted(status, key = lambda x: x[1], reverse = True)
 
-with open('/home/eric/projects/svc-status/status-results.csv','w') as file:
+with open('/home/eric/projects/svc-status/status-results.csv','w') as file: # change the path where to save the result
     writer = csv.writer(file)
     writer.writerows(status)
